8784977487ae62a2a696b0d36934c6cc3aa2010f
genku-suzuki <139086720+genku-suzuki@users.noreply.github.com>
2023-07-19 15:00:19 +0900

    Add files via upload

---
+++ kadai_011 (2).py
@@ -0,0 +1,12 @@
+num=0
+list = ["水","金","地","火","木","土","天","海","冥"]
+while num <= 10:
+  print(list[num])
+  num+=1
+  if num>8:
+    break
+
+ list = ["水","金","地","火","木","土","天","海","冥"]
+
+for list in list:
+ print(list) 
