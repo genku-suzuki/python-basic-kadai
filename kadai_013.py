d1e1bd930b9297b05caf6736a6666fdafdb142f8
genku-suzuki <139086720+genku-suzuki@users.noreply.github.com>
2023-07-20 12:07:46 +0900

    Add files via upload

---
+++ kadai_013.py
@@ -0,0 +1,8 @@
+import random
+from random import randint, randrange
+price = randint(1,10000)
+print("消費税抜き"+str(price)+"円の商品は消費税込みの場合")
+def calculate_total(price):
+  total = price * 1.1
+  print(f"{total}円です")
+calculate_total(price)
